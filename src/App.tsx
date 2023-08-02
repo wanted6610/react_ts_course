f2a40a0ebe7524c1a387a3105f4722b7d19ca643
unknown <vatocat_nn@mail.ru>
2023-08-02 23:41:53 +0300

    Разделение на темы + хук на переключение, отдельные чанки страниц

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, {Suspense, useContext} from 'react';
+import "./styles/index.scss";
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+const App = () => {
+
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={(toggleTheme)}>toggle</button>
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={"/about"} element={<AboutPageAsync />} />
+                    <Route path={"/"} element={<MainPageAsync />} />
+
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            <p>main page</p>
+        </div>
+    );
+};
+
+export default MainPage;
